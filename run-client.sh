d81fc355a001c45b8bcea722582adf47e058bf21
David <ddahl@nulltxt.se>
2018-10-23 23:20:04 -0400

    simplify server start script

--- run-client.sh
+++ run-client.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env bash
-
+echo "open another shell and: 'npm run client' or navigate to http://127.0.0.1:8889"
 ./node_modules/.bin/http-server ./client/ -a 127.0.0.1 -p 8889 &
-node ./client/start.js
