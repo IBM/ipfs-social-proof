2924db07732af5f8428dca77eec3b7a81be49bbf
Tyler Yasaka <contact@tyleryasaka.me>
2018-10-28 05:06:55 -0700

    Fix style bugs

--- client-choo/index.js
+++ client-choo/index.js
@@ -15,4 +15,5 @@ app.use(require('./stores/clicks'))
 app.route('/', require('./views/main'))
 app.route('/*', require('./views/404'))
 
-module.exports = app.mount('body')
+const tree = app.start()
+module.exports = document.body.appendChild(tree)
