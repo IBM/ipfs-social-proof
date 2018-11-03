acaee1e007314b8a1c48883ccaf00a9b5790b2d7
David <ddahl@nulltxt.se>
2018-11-03 12:27:26 -0400

    fix typo

--- src/example-auth.js
+++ src/example-auth.js
@@ -1,4 +1,4 @@
-// 1. replace githubToekn with your own 'read-only' token with limited scopes and no write access
+// 1. replace githubToken with your own 'read-only' token with limited scopes and no write access
 // 2. rename this module to auth.js
 module.exports = {
   githubToken: '1234567891011121314151617181920'
